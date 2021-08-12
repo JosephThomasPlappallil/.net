ea16b135ace36f94512bee46356f3705aebdddfa
JosephThomasPlappallil <jt287561@gmail.com>
2021-08-13 00:21:23 +0530

    First commit

---
+++ calc/calc/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    class Program
+    {   
+        public void Add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("sum of {0} , {1} is {2}",a,b,c);
+        }
+        public void Sub(int a, int b)
+        {
+            int c = a - b;
+            Console.WriteLine("Difference of {0} , {1} is {2}", a, b, c);
+        }
+        public void Mul(int a, int b)
+        {
+            int c = a * b;
+            Console.WriteLine("Product of {0} , {1} is {2}", a, b, c);
+        }
+        public void Div(int a, int b)
+        {
+            int c = a / b;
+            Console.WriteLine("Division of {0} , {1} is {2}", a, b, c);
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Eneter two numbers ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
